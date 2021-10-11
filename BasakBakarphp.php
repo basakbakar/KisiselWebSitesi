e57392235b394406cd7599bb60e34d386ebf42bf
basakbakar <basakbakar312@gmail.com>
2021-10-11 14:02:44 +0300

    Commit1

---
+++ BasakBakarphp.php
@@ -0,0 +1,9 @@
+<?php
+  echo $_POST['Ad'];
+  echo $_POST['Soyad'];
+  echo $_POST['Cinsiyet'];
+  echo $_POST['DogumTarihi'];
+  echo $_POST['Adres'];
+  echo $_POST['Email'];
+  echo $_POST['Mesaj'];
+?>
